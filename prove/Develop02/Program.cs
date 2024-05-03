3cbe40f599858f2d19fff02f4ea58b16528fa3ea
other-world <jamie@other-world.org>
2024-05-02 23:52:11 -0600

    Mid-week exercise

--- prove/Develop02/Program.cs
+++ prove/Develop02/Program.cs
@@ -1,5 +1,25 @@
 using System;
 
+class Journal
+{
+    List _entries ;
+
+}
+
+class Entry
+{
+    string _date;
+    string _promptText;
+    string _entryText;
+
+    
+}
+
+class PromptGenerator
+{
+
+}
+
 class Program
 {
     static void Main(string[] args)
